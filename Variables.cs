30e18ab3efb0700921ed2365f68c285d56bda80d
Natalie Riien <natalieriien@gmail.com>
2016-09-15 23:49:53 -0600

    Add files via upload

---
+++ Variables.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variables : MonoBehaviour {
+
+    int MyInt = 22;
+
+	void Start () {
+        MyInt = MultiplyByTwo(MyInt);
+        Debug.Log(MyInt);
+	}
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = MyInt * 2;
+        return ret;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
