19c680779eb146682024243327983f36bd8505e2
Natalie Riien <natalieriien@gmail.com>
2016-11-10 13:14:39 -0700

    Conventions And Syntax

---
+++ ConventionsAndSyntax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+
+	//This is a comment
+
+	void Start ()
+    {
+        Debug.Log("The dot operator allows me to access Log within Debug"); //<-The semicolon ends this statement. Any statements between opening and closing curly braces need a semicolon.
+	}
+	
+    //Indenting keeps the code legible and organized.
+} //<-Curly braces start and end groups of code.
