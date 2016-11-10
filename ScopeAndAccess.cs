ec2b553ce1fe332faca9ad5b5a51ef24c0b1ceae
Natalie Riien <natalieriien@gmail.com>
2016-11-10 15:49:50 -0700

    ScopeAndAccess

---
+++ ScopeAndAccess.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccess : MonoBehaviour
+{
+
+    public int books = 5; //this variable can be used by other scripts because it is public
+    private int pens = 10; //this variable cannot because it is private
+
+	void Start () {
+        print("I have " + books + " books and " + pens + " pens.");
+	}
+	
+}
