b2a9563e829abc2113257bc8307bd014c4afb808
Natalie Riien <natalieriien@gmail.com>
2016-11-10 13:41:40 -0700

    SwitchStatement

---
+++ SwitchStatement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchStatement : MonoBehaviour
+{
+
+    public int PianoKeys;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PianoKeys = Random.Range(1, 3);
+        switch(PianoKeys)
+        {
+            case 1:
+                Debug.Log("Do");
+                break;
+            case 2:
+                Debug.Log("Re");
+                break;
+            case 3:
+                Debug.Log("Mi");
+                break;
+        }
+	}
+}
