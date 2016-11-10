5d1d1f6cb209917c2e26bd9782ad7906b9441bbb
Natalie Riien <natalieriien@gmail.com>
2016-11-10 12:55:01 -0700

    FunctionsAssignment

---
+++ Functions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Functions : MonoBehaviour
+{
+
+    int Flies;
+
+    void FliesGather()
+    {
+        Input.GetKeyDown(KeyCode.UpArrow);
+        Flies++;
+        print("There are now" + Flies + "Flies.");
+    }
+
+    void Update()
+    {
+        FliesGather();
+    }
+}
