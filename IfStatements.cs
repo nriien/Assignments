a3dda43c8b660590ac8a77b9fc1d5e48c10448d2
Natalie Riien <natalieriien@gmail.com>
2016-11-10 13:41:25 -0700

    If Statement

---
+++ IfStatements.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour
+{
+
+    public int Love;
+    public int Hate;
+
+	void Start ()
+    {
+	    if(Love > 10)
+        {
+            print("I'm in love!");
+        }
+        
+        else if(Hate > 10)
+        {
+            print("I hate them!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
